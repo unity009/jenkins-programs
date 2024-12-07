7ba6e4b125dbafdae3de26ff063df4eb5b98c66e
TechJunction <formanoj36@gmail.com>
2024-12-07 12:44:01 +0530

    test commit in one go

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public static void main(String[] args) {
     System.out.println("THis is java program");
     System.out.println(new java.util.Date());
     System.out.println("This is the to check to test autobuild.");
+    System.out.println("java commit in one go");
 
 
 }
--- NodeMain.js
+++ NodeMain.js
@@ -1,3 +1,4 @@
 console.log("This is node file.");
 console.log("Going to run using jenkins");
 console.log("Good,Keep it up.git");
+console.log("Node,commit in one go");
