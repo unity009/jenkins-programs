c3bce6e3ca8de8d129f057247d5cbbf2f0871362
TechJunction <formanoj36@gmail.com>
2024-12-07 12:20:23 +0530

    second test jenkins

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 
     System.out.println("THis is java program");
     System.out.println(new java.util.Date());
+    System.out.println("This is the to check to test autobuild.");
 
 
 }
